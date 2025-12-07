fae81548862618676f2804648fb9a22c8c9576ab
Areebashah <mehreebshah25@gmail.com>
2025-12-07 06:57:18 +0500

    Configure deploymentBranch to main

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'syedaareebashah', // Usually your GitHub org/user name.
   projectName: 'physical-ai-book', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
